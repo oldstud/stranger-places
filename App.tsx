05a4842c23fe2cf71765c484647eb6113b4163a3
Dima <respect4me@ukr.net>
2021-10-14 20:45:06 +0300

    init project first commit

---
+++ App.tsx
@@ -0,0 +1,20 @@
+
+import React from 'react';
+
+import {
+  SafeAreaView,
+  Text,
+} from 'react-native';
+
+
+const App = () => {
+
+  return (
+    <SafeAreaView>
+     <Text>Welcome</Text>
+    </SafeAreaView>
+  );
+};
+
+
+export default App;
